b022fa6a5c8b6cc52602c08af86366771a70642e
Alexander Kojevnikov <alexander@kojevnikov.com>
2009-09-09 01:26:06 +1000

    first working version

--- src/index.py
+++ src/index.py
@@ -1,11 +1,29 @@
 import os
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
-from pygments.lexers import get_all_lexers
+from pygments import highlight
+from pygments.lexers import get_all_lexers, get_lexer_by_name
+from pygments.formatters import HtmlFormatter
 
 class IndexHandler(webapp.RequestHandler):
     def get(self):
         path = os.path.join(os.path.dirname(__file__), 'templates/index.html')
+
+        code = self.request.get("code")
+        if not code:
+            # read from cookies
+            code = "print 'hello world!'"
+        lexer = self.request.get("lexer")
+        if not lexer:
+            lexer = "python"
         lexers = [(l[1][0], l[0]) for l in get_all_lexers()]
         lexers = sorted(lexers, lambda a, b: cmp(a[1].lower(), b[1].lower()))
-        self.response.out.write(template.render(path, {'lexers': lexers}))
+
+        formatter = HtmlFormatter(noclasses=True,
+                                  cssclass='',
+                                  cssstyles="overflow:auto;width:auto;border:solid#959596;white-space:pre;border-width:.1em .1em .1em .8em;padding:.2em .6em;")
+        html = highlight(code, get_lexer_by_name(lexer), formatter)
+        self.response.out.write(template.render(path, locals()))
+
+    def post(self):
+       self.get() 
