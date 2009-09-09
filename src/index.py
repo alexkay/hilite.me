4d981762637e37b9f195d88bc7b7139dfe3fab82
Alexander Kojevnikov <alexander@kojevnikov.com>
2009-09-09 17:03:37 +1000

    styles

--- src/index.py
+++ src/index.py
@@ -4,6 +4,7 @@ from google.appengine.ext.webapp import template
 from pygments import highlight
 from pygments.lexers import get_all_lexers, get_lexer_by_name
 from pygments.formatters import HtmlFormatter
+from pygments.styles import get_all_styles
 
 class IndexHandler(webapp.RequestHandler):
     def get(self):
@@ -18,12 +19,23 @@ class IndexHandler(webapp.RequestHandler):
             lexer = "python"
         lexers = [(l[1][0], l[0]) for l in get_all_lexers()]
         lexers = sorted(lexers, lambda a, b: cmp(a[1].lower(), b[1].lower()))
+        style = self.request.get("style")
+        if not style:
+            style = "colorful"
+        styles = sorted(get_all_styles(), key=str.lower)
 
-        formatter = HtmlFormatter(noclasses=True,
+        formatter = HtmlFormatter(style=style,
+                                  noclasses=True,
                                   cssclass='',
-                                  cssstyles="overflow:auto;width:auto;border:solid#959596;border-width:.1em .1em .1em .8em;padding:.2em .6em;")
+                                  cssstyles=
+                                  "overflow:auto;"
+                                  "width:auto;"
+                                  "border:solid#959596;"
+                                  "border-width:.1em .1em .1em .8em;"
+                                  "padding:.2em .6em;",
+                                  prestyles="margin: 0")
         html = highlight(code, get_lexer_by_name(lexer), formatter)
         self.response.out.write(template.render(path, locals()))
 
     def post(self):
-       self.get() 
+        self.get() 
