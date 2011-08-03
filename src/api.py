5b14d604735cde41eb792b60b6ba48330303da68
OpenGrid <lech.wilczynski@gmail.com>
2011-08-03 23:47:43 +0300

    added option to use GET for AJAX requests if 'code' parameter non empty. This change caused code redundance. Need a fix.

--- src/api.py
+++ src/api.py
@@ -24,6 +24,17 @@ from tools import hilite_me, update_styles
 
 class ApiHandler(webapp.RequestHandler):
     def get(self):
+        if self.request.get('code') != '':
+        code = self.request.get('code')
+        lexer = self.request.get('lexer')
+        style = self.request.get('style')
+        linenos = self.request.get('linenos')
+        divstyles = update_styles(style, self.request.get('divstyles'))
+        html = hilite_me(code, lexer, style, linenos, divstyles)
+        self.response.headers["Content-Type"] = "text/plain"
+        self.response.out.write(html)
+
+        else:
         path = os.path.join(os.path.dirname(__file__), 'templates/api.txt')
         self.response.headers["Content-Type"] = "text/plain"
         self.response.out.write(template.render(path, {}))
